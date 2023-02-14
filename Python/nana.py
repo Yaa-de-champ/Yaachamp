8e8638942e88c6736129d5bd41ec961a29d39714
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-02-14 16:08:10 +0000

    dictionary(1)

--- Python/nana.py
+++ Python/nana.py
@@ -18,3 +18,65 @@ print(french)
 book_codes = ["FH2010", "BV1999", "LB2010"]
 books_2010 = [code for code in book_codes if code.count("2010") == 1]
 print(books_2010)
+
+# ------------------------------------------------
+personal_info = {
+    'name':'Nana Yaa Adomaa',
+    'ID': 10947512,
+    'age': 20,
+    'home':'Pantang'
+}
+personal_info['height'] = 67.45 #to add a new key with a value
+print(personal_info)
+
+personal_info = {
+    'name':'Nana Yaa Adomaa',
+    'ID': 10947512,
+    'age': 20,
+    'home':'Pantang'
+}
+del personal_info['age'] #to remove a key valued pair
+print(personal_info)
+
+print(len(personal_info))
+# -------------------------------------------------------
+
+if 'name' in personal_info:
+    print('key "name" is in the dictionary')
+else:
+    print('oops!')
+
+if 'nage' in personal_info:
+    print('key "name" is in the dictionary')
+else:
+    print('oops!')
+
+if 'email' not in personal_info:
+    print('the key "email" is not in the dictionary')
+
+new_string = str(personal_info)
+print(new_string)
+
+print(type(personal_info))
+
+sorted_keys = sorted(personal_info)
+print(sorted_keys) #sort the keys in the dictionary
+
+items = personal_info.items()
+print(items)
+
+that_name = personal_info.get('name')
+print(that_name)
+
+ourFavFruits = dict([('Papa', 'Pawpaw'), ('Kwame', 'Orange'), ('Kobby', 'Mango')])
+print(ourFavFruits)
+
+# ----------------------------------------------------------------
+
+other_info = {
+    'email':'Yaadoku@gmail.com',
+    'contact': '0502317445'
+}
+personal_info.update(other_info)
+print(personal_info) #updates the dictionary
+
