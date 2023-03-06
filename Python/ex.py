e3e497dc7561ad150df750ba97e57a2744bbf2ff
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-03-06 11:00:06 +0000

    modules in python
    eg.statistics method

--- Python/ex.py
+++ Python/ex.py
@@ -1,225 +1,72 @@
-#print("Hello Quantum World!")
-#print("Hello Quantum World!")
-#print("nice to meet you")
-#print("not nice to meet you")
-#print("I am 18 years old")
-#print("I love music and playing games")
-#print("MCBROWN Kitchen is my favourite tv show")
-#course = "Introduction to Quantum Computing"
-#print(course)
-#age = "18"
-#print(age)
-#my_school = "Methodist Girls High School"
-#print(my_school)
-#my_favourite_number = str(67)
-#print(my_favourite_number)
-# my_name = "Nana Yaa"
-# print(" My name is "+ my_name )
-#print(3+2)
-#print(3.0 + 2)
-#print(3*2)
-#print(3/2)
-#x=5
-#print(x+5)
+# import math
+# value = math.pi
+# square = math.sqrt(16)
+# print(f'The value of pi is:{value}')
+# print(f'The square root of 16 is: {square}')
 
-# #print("hope")
-# print(" My favourite number is " + str(8) )
-# class_size = str(10)
-# print(" size of class: " + class_size)
-# state = "Accra"
-# print(" I am from " + state)
+# # import pygal
+# # pie = pygal.Pie()
+# # pie.title = 'Time spent on social networks'
+# # pie.add('Twitter', 47)
+# # pie.add('Facebook', 23)
+# # pie.add('Instagram', 30)
 
-#we use ctrl+/ to create a comment and uncomment
+# # pie.render_in_browser()
 
-# num_bananas = 2
-# num_apples = 5
-# NUMBER_OF_FRUITS = str (num_apples + num_bananas)
-# print(" the number of fruits is " + NUMBER_OF_FRUITS)
+# '''
+# help(math) #used to find the functionality of a module 
+# '''
 
-# counter = 1 #starts numbering from 1 to 3  
-# while counter < 4:
-# 	print(counter)
-# 	counter += 1
-# this is some form of numbering 
+# import math
+# print('Rounded up to the nearest number')
+# rounded = math.ceil(22.7324)
+# print(rounded)
 
 
-# counter = 5   #starts numbering from 5 to 9
-# while counter < 10:
-# 	print(counter)
-# 	counter += 1
-
-# counter = 5  #starts numbering from 6 to 10
-# while counter < 10:
-#   counter += 1
-#   print(counter)
-
-# num_of_goods = 1
-# while num_of_goods < 5:
-# 	num_of_goods += 1
-# 	print("add registry...")
-
-# end 
-# expecting the Add registry to be printed 4 times 
-
-#to create a for loop, we add the keyword "for", and a variable i and the word in and finally range() 
-# for i in range():
-#in a for loop we can specify how many times we'd like our loop to run with the range()
-
-# for i in range(4):
-# 	print("HAPPY BIRTHDAY!...")
-
-# for i in range(3): #numbering starts from zero to 2
-# 	print(i)
+# import statistics
+# scores = [90, 45, 88, 67, 88]
+# mean =statistics.mean(scores)
+# print(f'Mean score is {mean}')
 
 
-# to_do = ["read","workout","code"]
-# print(to_do)
-
-# #we can also create an empty list
-# to_do = []
-# print (to_do)
-
-#value of a list are called elements
-
-# for i in range(3):
-# 	print(i)
-# 	print("for loops are great!")
-
-# for i in range(3): # i is the counter variable 
-# 	print("go girl!!..")
-
-# for i in  range(3):
-# 	print("we rock!...")
-# print("we run the world...")
-
-# i is not a constant variable , we can use other variables like x , y etc
-
-# for x in  range (3) :
-# 	print("counts")
-# print(x)
-
-# this prints counts 3 times and include the numeber 2
+# import statistics, math
+# diameters = [9, 7, 8, 4]
+# result = statistics.mean(diameters)
+# print(f'Mean diameter is {result}')
+# print('value of pi is:')
+# print(math.pi)
 
 
-# for x in  range (3) :
-#  print("counts")
-#  print(x)
-
-# this prints counts followed by the number zero and another count followed by the number 1
-# the indention creates the difference
-
-#creating a list 
-
-# activities = ["joggings", "washing dishes", "reading"]
-# print(activities)
-
-# jobs = []
-# print(jobs)
-
-# students_names = ["Nana Yaa", "Calista", "Asabea", "Kukua"]
-# print(students_names[0])
-# print(students_names[1])
-# print(students_names[3])
-
-# random_numbers = [23, 65, 45, 18, 10]
-# random_numbers[0] = 34
-# print(random_numbers)
-
-# marks = [87, 91, 53]
-# marks.append(45)
-# print(marks)
-
-# BMEN_names = ["James", "Cecil", "Safiu"]
-# BMEN_names.append("Mary")
-# print(BMEN_names
-
-# shopping = ["long dress", "black shoe", "bag"]
-# shopping.insert(0, "watch")
-# print(shopping)
+# from math import pi
+# print('value of pi is:')
+# print(pi)
 
 
-# shopping = ["long dress", "black shoe", "bag"]
-# shopping.insert(0, "watch")
-# shopping.insert(1, "stationery")
-# print(shopping)
-
-#to remove the last element we use .pop()
-
-# BMEN_names = ["James", "Cecil", "Safiu"]  
-# BMEN_names.pop()
-# print(BMEN_names)  #so in the output Safiu was removed
-
-# len() is used to count the number of element we have in a list 
-
-# VAR6 = "you"
-# print(VAR6)
-
-# for i in range(3):
-#     print("hey")
-#     print("hope you good")
-
-# for index in range(4):
-#     print("happy birthday")
-# range always starts with zero
-
-# my_list = ["H", "i", "!"]
-# for index in range(3):
-#     print(my_list[index]) #prints once at a time on different lines
-
-# for yaa in range(4):
-#     print(yaa)
-#     # index can be any name like yaa
-
-# my_list = ["H", "i", "!"]
-# for index in range(3):
-#     print(my_list[index], end='')
-# # prints Hi!
-
-# my_list = ["H", "i", "!"]
-# for index in range(3):
-#     print(my_list[index], end=' ') 
-# prints H i !
+# from statistics import mean
+# test_scores = [33, 7, 4, 6]
+# result = mean(test_scores)
+# print(f'Mean result is {result}')
 
 
-# my_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
+# from statistics import mean
+# ages = [22, 26, 18, 35]
+# result = mean(ages)
+# print(f'Mean age is {result}')
 
-# for index in range(12):
-    # print( "here is a string" + " " + str(my_list[index]) ) #OR
-   #print( "here is a string" + " " + my_list[index] )
-# this " " is for leaving a space between the sentence and alphabets
 
- 
-# my_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
+# import statistics as stats
+# sales = [23, 34, 56, 67, 78, 98, 47, 68]
+# median = stats.median(sales)
+# print(median)
 
-# for index in range(12):
-#     print("index: " + str(index) )
-#     print("element: " + my_list[index] )
+# import math as math_constants
 
-# my_list = [-1, 0, 52.3, 2, 0]
+# math = 'Grade 12 constants'
+# print(math)
+# print('pi:')
+# print(math_constants.pi)
+# print("Euler's constants:")
+# print(math_constants.e)
 
-# for i in range(5):
-#     if my_list[i] != 0:
-#        print(my_list[i])
-
-# my_list = [-53, 28, 19]
-
-# if my_list[0] < 0:
-#     my_list[0] = my_list[0] * -1
-
-# print(my_list)
-
-# my_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-# total = 0
-
-# for i in my_list:
-#     total += i
-# print(total)
-# summing up all elements in a list.
-
-# my_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-
-# for i in range(10):
-#     my_list[i] = my_list[i] - 10
-    
-# print(my_list)
-# subtracting 10 from each element in
+import math as m
+print(m.floor(44.32))
