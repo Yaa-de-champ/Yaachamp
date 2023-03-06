0cb203f1942a7d762cdd199e12ae1af0eda95e97
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-03-06 11:06:03 +0000

    Update ex1.c

--- C programming/ex1.c
+++ C programming/ex1.c
@@ -131,38 +131,38 @@
 
 
 
-// // It is also a best practice to use all upper case letters when declaring a constant
-// int main() {
+// It is also a best practice to use all upper case letters when declaring a constant
+int main() {
   
-// // Speed of light is 1.86e5 mi/s or 186000 mi/s we will store 1.86 and do the multiplicaiton later.
-//   const double SPEEDOFLIGHT = 1.86;
-//   int timeTraveledInSeconds = 30;
+// Speed of light is 1.86e5 mi/s or 186000 mi/s we will store 1.86 and do the multiplicaiton later.
+    const double SPEEDOFLIGHT = 1.86;
+    int timeTraveledInSeconds = 30;
 
 
-//   // No need to change below here
+    //  No need to change below here
 //   printf("Light would travel %.2f miles in %d seconds\n", SPEEDOFLIGHT * 100000 * timeTraveledInSeconds, timeTraveledInSeconds);
 
-// }
+}
 
-// int main() {
-//   int pin = 0;
-//   int tries = 0;
+int main() {
+    int pin = 0;
+    int tries = 0;
 
-//   printf("BANK OF CODECADEMY:\n");
-//   printf("Enter your PIN: ");
-//   scanf("%d", &pin);
+    printf("BANK OF CODECADEMY:\n");
+    printf("Enter your PIN: ");
+    scanf("%d", &pin);
 
-//   tries++;
+    tries++;
 
-//   while (pin != 1234 && tries < 3) {
-//     printf("Enter your PIN: ");
-//     scanf("%d", &pin);
-//     tries++;
-//   }
+    while (pin != 1234 && tries < 3) {
+    printf("Enter your PIN: ");
+    scanf("%d", &pin);
+    tries++;
+}
 
-//   if (pin == 1234) {
-//     printf("PIN accepted!\n");
-//     printf("You now have access.\n");
-//   }
-// }
+    if (pin == 1234) {
+    printf("PIN accepted!\n");
+    printf("You now have access.\n");
+    }
+}
 
