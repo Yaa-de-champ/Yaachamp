1331fe8b935123729d7a2b8cc3e957742d34bf65
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-05-09 12:09:19 +0000

    working with dash to create bar plot

--- Python/app.py
+++ Python/app.py
@@ -88,3 +88,63 @@ class Game:
 
 game = Game()
 game.start()
+
+
+# %%
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+
+#The file is encoded using ISO-8859-1 character encoding, which is a standard way of representing characters in the file.
+#We defined data type of specific columns such as (Div1Airport, Div1TailNum, Div2Airport, and Div2TailNum) to be strings, which ensures that these columns are read as text instead of numbers.
+airline_dataset = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+                                encoding='ISO-8859-1',
+                                dtype={'Div1Airport':str, 'Div1TailNum':str, 'Div2Airport':str, 'Div2TailNum':str})
+
+#create a dash aplication layout
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[html.H1('Total number of flights to the destination state split by reporting airline',
+                                        style={'textAlign':'center','color':'#503D36', 'font-size':40}),
+                                html.Div(['Input Year: ', dcc.Input(id = 'input-year', value='2010',
+                                                                    type='number', style={'height':'50px', 'font-size':35}),],
+                                style={'font-size':40}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(dcc.Graph(id = 'bar-plot')),
+                                ])
+
+#adding callback decorator
+@app.callback( Output(component_id='bar-plot', component_property='figure'), 
+                Input(component_id='input-year', component_property='value'))
+
+#add computation to callback function and return graph
+def get_graph(entered_year):
+
+    
+        df =  airline_dataset[airline_dataset['Year']==int(entered_year)]
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        bar_data = df.groupby('DestState')['Flights'].sum().reset_index()
+        fig = px.bar(bar_data, x= "DestState", y= "Flights", title='Total number of flights to the destination state split by reporting airline') 
+        fig.update_layout(title='Flights to Destination State', xaxis_title='DestState', yaxis_title='Flights')
+        return fig
+
+#-------------------------------------------------or------------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+        # bar_data = df.groupby('DestState')['Flights'].sum().reset_index()
+        # fig = px.bar(bar_data, x= "DestState", y= "Flights", title='Total number of flights to the destination state split by reporting airline') 
+        # fig.update_layout(title='Total number of flights to the destination state split by reporting airline', xaxis_title='States', yaxis_title='Flights')
+        # return fig
+
+#run the app
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+# %%
